8b5137af00f89ff07638258879c3e7606ffe281a
Zengliwei <zengliwei@163.com>
2020-01-17 09:58:20 +0800

    # debug

--- start.cmd
+++ start.cmd
@@ -1,5 +1,4 @@
 @echo off
-
 echo rename %CD%\config\router\*.conf *. > "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\stop-docker-projects.cmd"
-
-docker-compose up
+cd ./src
+docker-compose up -d

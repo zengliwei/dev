2aa250c48586a33b3b411d7656d553a276240e65
zengliwei <zengliwei@163.com>
2020-01-24 09:39:59 +0800

    ^ change structure

--- start.cmd
+++ start.cmd
@@ -1,4 +1,3 @@
 @echo off
 echo rename %CD%\config\router\*.conf *. > "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\stop-docker-projects.cmd"
-cd ./src
 docker-compose up -d

42a824859ae8172c425f1f76b30f9a01337e7a89
zengliwei <zengliwei@163.com>
2020-01-29 23:56:57 +0800

    ^ upgrade

--- start.cmd
+++ start.cmd
@@ -1,3 +1,25 @@
 @echo off
-echo rename %CD%\config\router\*.conf *. > "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\stop-docker-projects.cmd"
-docker-compose up -d
+
+::::
+:: Create batch down site script
+::
+echo rename %CD%\config\router\*.conf *. > %UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\stop-docker-projects.cmd
+
+::::
+:: Add host mapping
+::
+set hostMappingSet=0
+for /f "tokens=1,2" %%i in ( %SystemRoot%\System32\drivers\etc\hosts ) do (
+  if %%j == localhost set hostMappingSet=1
+)
+if %hostMappingSet% == 1 exit
+echo. >> %SystemRoot%\System32\drivers\etc\hosts
+echo. >> %SystemRoot%\System32\drivers\etc\hosts
+echo 127.0.0.1 localhost >> %SystemRoot%\System32\drivers\etc\hosts
+echo 127.0.0.1 db.localhost >> %SystemRoot%\System32\drivers\etc\hosts
+echo 127.0.0.1 mail.localhost >> %SystemRoot%\System32\drivers\etc\hosts
+
+::::
+:: Create containers
+::
+docker-compose up --no-recreate -d

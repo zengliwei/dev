14eabb60a260fac7728d1afb56801bb8462bc4b1
zengliwei <zengliwei@163.com>
2020-03-27 22:11:03 +0800

    ^ change for code style PSR-12

--- config/phpmyadmin/config.inc.php
+++ config/phpmyadmin/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-require( '/etc/phpmyadmin/config.secret.inc.php' );
+require '/etc/phpmyadmin/config.secret.inc.php';
 
 /* Ensure we got the environment */
 $vars = [
@@ -23,67 +23,64 @@ $vars = [
     'PMA_CONTROLPASS',
     'PMA_QUERYHISTORYDB'
 ];
-foreach ( $vars as $var ) {
-    $env = getenv( $var );
-    if ( !isset( $_ENV[$var] ) && $env !== false ) {
+foreach ($vars as $var) {
+    $env = getenv($var);
+    if (!isset($_ENV[$var]) && $env !== false) {
         $_ENV[$var] = $env;
     }
 }
-if ( isset( $_ENV['PMA_QUERYHISTORYDB'] ) ) {
-    $cfg['QueryHistoryDB'] = boolval( $_ENV['PMA_QUERYHISTORYDB'] );
+if (isset($_ENV['PMA_QUERYHISTORYDB'])) {
+    $cfg['QueryHistoryDB'] = boolval($_ENV['PMA_QUERYHISTORYDB']);
 }
 
 /* Arbitrary server connection */
-if ( isset( $_ENV['PMA_ARBITRARY'] ) && $_ENV['PMA_ARBITRARY'] === '1' ) {
+if (isset($_ENV['PMA_ARBITRARY']) && $_ENV['PMA_ARBITRARY'] === '1') {
     $cfg['AllowArbitraryServer'] = true;
 }
 
 /* Play nice behind reverse proxys */
-if ( isset( $_ENV['PMA_ABSOLUTE_URI'] ) ) {
-    $cfg['PmaAbsoluteUri'] = trim( $_ENV['PMA_ABSOLUTE_URI'] );
+if (isset($_ENV['PMA_ABSOLUTE_URI'])) {
+    $cfg['PmaAbsoluteUri'] = trim($_ENV['PMA_ABSOLUTE_URI']);
 }
 
 /* Figure out hosts */
 
 /* Fallback to default linked */
-$hosts = [ 'db' ];
+$hosts = ['db'];
 
 /* Set by environment */
-if ( !empty( $_ENV['PMA_HOST'] ) ) {
-    $hosts = [ $_ENV['PMA_HOST'] ];
-    $verbose = [ $_ENV['PMA_VERBOSE'] ];
-    $ports = [ $_ENV['PMA_PORT'] ];
+if (!empty($_ENV['PMA_HOST'])) {
+    $hosts = [$_ENV['PMA_HOST']];
+    $verbose = [$_ENV['PMA_VERBOSE']];
+    $ports = [$_ENV['PMA_PORT']];
+} elseif (!empty($_ENV['PMA_HOSTS'])) {
+    $hosts = array_map('trim', explode(',', $_ENV['PMA_HOSTS']));
+    $verbose = array_map('trim', explode(',', $_ENV['PMA_VERBOSES']));
+    $ports = array_map('trim', explode(',', $_ENV['PMA_PORTS']));
 }
-else if ( !empty( $_ENV['PMA_HOSTS'] ) ) {
-    $hosts = array_map( 'trim', explode( ',', $_ENV['PMA_HOSTS'] ) );
-    $verbose = array_map( 'trim', explode( ',', $_ENV['PMA_VERBOSES'] ) );
-    $ports = array_map( 'trim', explode( ',', $_ENV['PMA_PORTS'] ) );
-}
-if ( !empty( $_ENV['PMA_SOCKET'] ) ) {
-    $sockets = [ $_ENV['PMA_SOCKET'] ];
-}
-else if ( !empty( $_ENV['PMA_SOCKETS'] ) ) {
-    $sockets = explode( ',', $_ENV['PMA_SOCKETS'] );
+if (!empty($_ENV['PMA_SOCKET'])) {
+    $sockets = [$_ENV['PMA_SOCKET']];
+} elseif (!empty($_ENV['PMA_SOCKETS'])) {
+    $sockets = explode(',', $_ENV['PMA_SOCKETS']);
 }
 
 /* Server settings */
-for ( $i = 1; isset( $hosts[$i - 1] ); $i++ ) {
+for ($i = 1; isset($hosts[$i - 1]); $i++) {
     $cfg['Servers'][$i]['host'] = $hosts[$i - 1];
-    if ( isset( $verbose[$i - 1] ) ) {
+    if (isset($verbose[$i - 1])) {
         $cfg['Servers'][$i]['verbose'] = $verbose[$i - 1];
     }
-    if ( isset( $ports[$i - 1] ) ) {
+    if (isset($ports[$i - 1])) {
         $cfg['Servers'][$i]['port'] = $ports[$i - 1];
     }
-    if ( isset( $_ENV['PMA_USER'] ) ) {
+    if (isset($_ENV['PMA_USER'])) {
         $cfg['Servers'][$i]['auth_type'] = 'config';
         $cfg['Servers'][$i]['user'] = $_ENV['PMA_USER'];
-        $cfg['Servers'][$i]['password'] = isset( $_ENV['PMA_PASSWORD'] ) ? $_ENV['PMA_PASSWORD'] : '';
-    }
-    else {
+        $cfg['Servers'][$i]['password'] = isset($_ENV['PMA_PASSWORD']) ? $_ENV['PMA_PASSWORD'] : '';
+    } else {
         $cfg['Servers'][$i]['auth_type'] = 'cookie';
     }
-    if ( isset( $_ENV['PMA_PMADB'] ) ) {
+    if (isset($_ENV['PMA_PMADB'])) {
         $cfg['Servers'][$i]['pmadb'] = $_ENV['PMA_PMADB'];
         $cfg['Servers'][$i]['relation'] = 'pma__relation';
         $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
@@ -105,22 +102,22 @@ for ( $i = 1; isset( $hosts[$i - 1] ); $i++ ) {
         $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
         $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
     }
-    if ( isset( $_ENV['PMA_CONTROLHOST'] ) ) {
+    if (isset($_ENV['PMA_CONTROLHOST'])) {
         $cfg['Servers'][$i]['controlhost'] = $_ENV['PMA_CONTROLHOST'];
     }
-    if ( isset( $_ENV['PMA_CONTROLPORT'] ) ) {
+    if (isset($_ENV['PMA_CONTROLPORT'])) {
         $cfg['Servers'][$i]['controlport'] = $_ENV['PMA_CONTROLPORT'];
     }
-    if ( isset( $_ENV['PMA_CONTROLUSER'] ) ) {
+    if (isset($_ENV['PMA_CONTROLUSER'])) {
         $cfg['Servers'][$i]['controluser'] = $_ENV['PMA_CONTROLUSER'];
     }
-    if ( isset( $_ENV['PMA_CONTROLPASS'] ) ) {
+    if (isset($_ENV['PMA_CONTROLPASS'])) {
         $cfg['Servers'][$i]['controlpass'] = $_ENV['PMA_CONTROLPASS'];
     }
     $cfg['Servers'][$i]['compress'] = false;
     $cfg['Servers'][$i]['AllowNoPassword'] = true;
 }
-for ( $i = 1; isset( $sockets[$i - 1] ); $i++ ) {
+for ($i = 1; isset($sockets[$i - 1]); $i++) {
     $cfg['Servers'][$i]['socket'] = $sockets[$i - 1];
     $cfg['Servers'][$i]['host'] = 'localhost';
 }
@@ -135,6 +132,6 @@ $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
 
 /* Include User Defined Settings Hook */
-if ( file_exists( '/etc/phpmyadmin/config.user.inc.php' ) ) {
-    include( '/etc/phpmyadmin/config.user.inc.php' );
+if (file_exists('/etc/phpmyadmin/config.user.inc.php')) {
+    include('/etc/phpmyadmin/config.user.inc.php');
 }
